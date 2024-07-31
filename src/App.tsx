7a663698e28bce85996e95b64caf80fa6cec7b15
Lucas <zubiarrainlucas@gmail.com>
2024-07-31 16:07:15 +0200

    deployement 2

--- src/App.tsx
+++ src/App.tsx
@@ -95,14 +95,14 @@ const App = () => {
                 className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
               >
                 <Home className="h-5 w-5" />
-                Les tendances
+                Visualiser les tendances
               </Link>
               <Link
                  to="/Meteo"
                 className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
               >
                 <ShoppingCart className="h-5 w-5" />
-                Meteo
+                Météo
                 <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                   6
                 </Badge>
